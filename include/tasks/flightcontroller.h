23315c7dc4b0dc3bfcdf2664eaddd6043a0dc73d
Horvath Gergo <gege32@gmail.com>
2018-04-03 22:03:01 +0200

    motors now working with calibration

--- include/tasks/flightcontroller.h
+++ include/tasks/flightcontroller.h
@@ -14,7 +14,7 @@
 #include "semihosting/Trace.h"
 #include "datamodels.h"
 
-#define MAX_MOTOR_THROTTLE 2000
+#define MAX_MOTOR_THROTTLE 1500
 #define MIN_MOTOR_THROTTLE 1000
 
 #define FRONT_LEFT_MOTOR_TIMER TIM_CHANNEL_1
@@ -41,5 +41,6 @@ uint32_t rear_right_throttle;
 uint32_t rear_left_throttle;
 
 void FlightControllerTask(void* const arguments);
+void CalibrateESC();
 
 #endif /* TASKS_FLIGHTCONTROLLER_H_ */
